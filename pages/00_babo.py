a502b117b04c0555bce4cb75232e6999e1a58f0a
하지현 <whale9524@gmail.com>
2025-06-05 15:46:30 +0900

    Update 00_babo.py

--- pages/00_babo.py
+++ pages/00_babo.py
@@ -1,2 +1,3 @@
 import streamlit as st
 st.image("https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExdWgyY3Q0eHJhNmcwMDNmbGZ6ZzI0NTl2MTdiM3E5dW95N2lzaXRtbSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/z4Ut4kYEMW0s8/giphy.gif")
+print("어 확실히 알겠어 ㅇㅇ")
