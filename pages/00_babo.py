58ac6be86a0fe032bef5cd8127108901607745b6
하지현 <whale9524@gmail.com>
2025-06-05 15:45:20 +0900

    Create 00_babo.py

---
+++ pages/00_babo.py
@@ -0,0 +1,2 @@
+import streamlit as st
+st.image("https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExdWgyY3Q0eHJhNmcwMDNmbGZ6ZzI0NTl2MTdiM3E5dW95N2lzaXRtbSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/z4Ut4kYEMW0s8/giphy.gif")
